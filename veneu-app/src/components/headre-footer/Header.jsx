65ab37f3bfc7febd0b400ad5a80954318305dbbc
unknown <mabbasidesign@yahoo.com>
2018-11-17 14:36:15 -0700

    header complete

--- veneu-app/src/components/headre-footer/Header.jsx
+++ veneu-app/src/components/headre-footer/Header.jsx
@@ -15,6 +15,19 @@ class Header extends Component {
         headerShow: false
     }
 
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        // console.log('user scrolling');
+        if(window.scrollY > 0){
+            this.setState({ headerShow: true });
+        }
+        else{
+            this.setState({ headerShow: false });
+        }
+    }
 
     toggleDrawer = (value) => {
         this.setState({
@@ -27,7 +40,7 @@ class Header extends Component {
             <AppBar
                 position="fixed"
                 style={{
-                    backgroundColor: '#2f2f2f',
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                     boxShadow: 'none',
                     padding: '10px 0px'
                 }}

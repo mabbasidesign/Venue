3f032b88f916b5df0950b72a2f02933b9de61e41
unknown <mabbasidesign@yahoo.com>
2018-11-17 12:31:24 -0700

    creating header

--- veneu-app/src/App.js
+++ veneu-app/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './resources/styles.css'
+import Header from './components/headre-footer/Header';
+import Footer from './components/headre-footer/footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       Hello
+        <Header />
       </div>
     );
   }

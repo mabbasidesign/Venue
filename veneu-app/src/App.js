638594c2e79991424a87160799be4547217f3d10
unknown <mabbasidesign@yahoo.com>
2018-11-18 10:31:28 -0700

    add pricing component

--- veneu-app/src/App.js
+++ veneu-app/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/headre-footer/footer';
 import Featured from './components/featured/index';
 import VunueNfo from './components/venueInfo/index';
 import Highlights from './components/Highlights/index';
+import Pricing from './components/pricing/index';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Featured />
         <VunueNfo />
         <Highlights />
+        <Pricing />
       </div>
     );
   }
